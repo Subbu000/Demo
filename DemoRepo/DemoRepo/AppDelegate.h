074a5fe3ee2fbedbe4dc18fa2505b9efd2d675da
Subbarao-SI <venkata@sportzinteractive.net>
2016-07-07 14:15:51 +0530

    Initial project Commit

---
+++ DemoRepo/DemoRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoRepo
+//
+//  Created by Venkata Subbarao on 07/07/16.
+//  Copyright © 2016 Venkata Subbarao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
